fba2076094fbd090fd6fe180cc619089f8f344fe
skan0421 <skan0421@gmail.com>
2023-04-18 19:37:41 +0900

    제발 진짜

---
+++ src/main/java/com/team/pj/SpringBootTeamPj126Application.java
@@ -0,0 +1,13 @@
+package com.team.pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTeamPj126Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTeamPj126Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/pj/SpringBootTeamPj126ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTeamPj126ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
